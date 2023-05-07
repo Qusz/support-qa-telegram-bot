fe20b97ad3e0063af5d549a0c4f41a415951f55b
Qusz <de.gorchakov@gmail.com>
2023-05-07 22:20:39 +0300

    add type

---
+++ src/types/message.ts
@@ -0,0 +1,11 @@
+import type { Chat } from 'grammy/types';
+
+export interface Message {
+  chat: Chat.PrivateChat | Chat.GroupChat | Chat.SupergroupChat;
+  title: string | undefined;
+  messageAuthor: string | undefined;
+  messageId: number;
+  text: string | undefined;
+  date: number;
+  isSupport: boolean | null;
+}
