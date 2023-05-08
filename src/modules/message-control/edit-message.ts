765b47b1c8317ced83f3a99dad0befee2c63bbfd
Qusz <de.gorchakov@gmail.com>
2023-05-08 11:14:45 +0300

    improve attachments handling logic

--- src/modules/message-control/edit-message.ts
+++ src/modules/message-control/edit-message.ts
@@ -1,16 +1,23 @@
 import type { MainContext } from '@/types';
 
 import updateMessages from '@/modules/db-control/update-messages';
+import hasAttachments from './has-attachments';
 
 export default async function (ctx: MainContext) {
-  // TODO Fix type guard
-
   const messageId = ctx.update.edited_message?.message_id;
   const payload = ctx.update.edited_message?.text;
 
-  if (payload && messageId) {
-    await updateMessages(messageId, payload);
-  } else if (messageId && !payload) {
-    await updateMessages(messageId, 'ERROR: Message update failed');
+  switch (true) {
+    case payload && messageId && !hasAttachments(ctx):
+      await updateMessages(messageId, payload);
+      break;
+    case payload && messageId && hasAttachments(ctx):
+      await updateMessages(messageId, 'Вложение');
+      break;
+    case !payload && messageId && hasAttachments(ctx):
+      await updateMessages(messageId, 'Вложение');
+      break;
+    default:
+      throw new Error('UNEXCEPTED ERROR: Cannot update message');
   }
 }
