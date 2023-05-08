852207574286de05d1d3302d98369b9c4635685a
Qusz <de.gorchakov@gmail.com>
2023-05-08 11:10:54 +0300

    add check for every type of attachment & handle updated message

--- src/modules/message-control/has-attachments.ts
+++ src/modules/message-control/has-attachments.ts
@@ -1,9 +1,29 @@
 import { MainContext } from '@/types';
 
 export default function (ctx: MainContext) {
-  if (!ctx.message) {
-    console.error('ERROR: Invalid message instance');
-    return undefined;
+  const message = ctx.message ?? ctx.editedMessage;
+
+  if (!message) {
+    throw new Error('ERROR: Cannot access message instance');
   }
-  return ctx.message.document;
+
+  return !!(
+    message.audio ||
+    message.animation ||
+    message.document ||
+    message.photo ||
+    message.sticker ||
+    message.video ||
+    message.video_note ||
+    message.voice ||
+    message.contact ||
+    message.location ||
+    message.poll ||
+    message.dice ||
+    message.game ||
+    message.invoice ||
+    message.successful_payment ||
+    message.passport_data ||
+    message.proximity_alert_triggered
+  );
 }
