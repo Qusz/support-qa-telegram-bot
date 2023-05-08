7d3e4c9c028d77ace2d38fa64451b24cd05b50d5
Qusz <de.gorchakov@gmail.com>
2023-05-08 12:02:00 +0300

    update import

--- src/utils/is-support-account.ts
+++ src/utils/is-support-account.ts
@@ -1,4 +1,4 @@
-import SUPPORT_USERS from '@/constants/users';
+import { SUPPORT_USERS } from '@/constants/users';
 
 export default function (username: string) {
   return SUPPORT_USERS.includes(username);
