1953bbd536c3929c03f716de319fc047e3137563
Qusz <de.gorchakov@gmail.com>
2023-05-08 20:26:30 +0300

    minor db model tweaks

--- src/modules/db-control/db-model.ts
+++ src/modules/db-control/db-model.ts
@@ -6,40 +6,57 @@ export const sequelize = new Sequelize({
   storage: config.get('db-path')
 });
 
-export const messagesTable = sequelize.define('messages', {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-    allowNull: false
+export const messagesTable = sequelize.define(
+  'messages',
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    },
+    author: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    chat_title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    sent_at: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+    is_premium_bonus: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false
+    },
+    response_time: {
+      type: DataTypes.FLOAT
+    },
+    message_text: {
+      type: DataTypes.STRING
+    },
+    message_id: {
+      type: DataTypes.NUMBER,
+      allowNull: false
+    },
+    replied_to_message: {
+      type: DataTypes.NUMBER
+    },
+    created_at: {
+      type: DataTypes.DATE,
+      field: 'created_at',
+      defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+    },
+    updated_at: {
+      type: DataTypes.DATE,
+      field: 'updated_at',
+      defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+    }
   },
-  author: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  chat_title: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  sent_at: {
-    type: DataTypes.DATE,
-    allowNull: false
-  },
-  is_premium_bonus: {
-    type: DataTypes.BOOLEAN,
-    allowNull: false
-  },
-  response_time: {
-    type: DataTypes.FLOAT
-  },
-  message_text: {
-    type: DataTypes.STRING
-  },
-  message_id: {
-    type: DataTypes.NUMBER,
-    allowNull: false
-  },
-  replied_to_message: {
-    type: DataTypes.NUMBER
+  {
+    createdAt: 'created_at',
+    updatedAt: 'updated_at'
   }
-});
+);
