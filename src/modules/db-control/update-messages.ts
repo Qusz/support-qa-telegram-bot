7e53cdac1c63ca991af805a02c0633121ec4adaf
Qusz <de.gorchakov@gmail.com>
2023-05-08 11:20:44 +0300

    simplify function

--- src/modules/db-control/update-messages.ts
+++ src/modules/db-control/update-messages.ts
@@ -1,22 +1,22 @@
 import { messagesTable } from './db-model';
 
 export default async function (
-  messageId: number,
-  newText: string
-): Promise<void> {
+  messageId: number | undefined,
+  newText: string | undefined
+) {
   const message = await messagesTable.findOne({
     where: { message_id: messageId }
   });
 
-  if (message) {
-    try {
-      await message.update({ message_text: newText });
-    } catch (error) {
-      if (error instanceof Error) {
-        throw new Error(`Error updating message: ${error.message}`);
-      }
+  if (!message) {
+    throw new Error(`Message ${messageId} not found.`);
+  }
+
+  try {
+    await message.update({ message_text: newText });
+  } catch (error) {
+    if (error instanceof Error) {
+      throw new Error(`Error updating message: ${error.message}`);
     }
-  } else {
-    console.log(`Message ${messageId} not found.`);
   }
 }
