10ae2fe00a9b80e0b32eb5c94ef8bdf6d5a0a140
Qusz <de.gorchakov@gmail.com>
2023-06-07 18:46:50 +0300

    update message model

--- src/types/message.ts
+++ src/types/message.ts
@@ -2,6 +2,7 @@ import type { Chat } from 'grammy/types';
 
 export interface Message {
   chat: Chat.PrivateChat | Chat.GroupChat | Chat.SupergroupChat;
+  chatId: number;
   title: string | undefined;
   messageAuthor: string | undefined;
   messageId: number;
