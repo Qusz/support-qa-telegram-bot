b4532c1d93e7b8c12b32a9dacf319f897a120b1e
Qusz <de.gorchakov@gmail.com>
2023-06-07 18:48:19 +0300

    add check for exteded support hours

--- src/utils/working-hours-check.ts
+++ src/utils/working-hours-check.ts
@@ -1,11 +1,15 @@
-import { WORKING_HOURS } from '@/constants/common';
+import { WORKING_HOURS, WORKING_HOURS_EXTENDED } from '@/constants/common';
+import { EXTENDED_SUPPORT_CUSTOMERS } from '@/constants/extended-support';
 import epochToTimestamp from './epoch-to-timestamp';
 
-export default function (epochValue: number) {
+export default function (chatId: number, epochValue: number) {
   const hours = epochToTimestamp(epochValue).getUTCHours();
 
-  if (hours < WORKING_HOURS.close) {
-    return true;
+  if (EXTENDED_SUPPORT_CUSTOMERS.includes(chatId)) {
+    if (hours < WORKING_HOURS_EXTENDED.close) return true;
+    return false;
   }
+
+  if (hours < WORKING_HOURS.close) return true;
   return false;
 }
